316277e18e33049ada000ea39491dedf027e78e8
Luckydipper <qhrqufdlek@naver.com>
2020-11-10 12:53:27 +0900

    [add] - first page default

--- flutter/test_app/lib/main.dart
+++ flutter/test_app/lib/main.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:test_app/screen/screen_home.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  //Stateless Widget.. 상태가 없는 위젯
+  //Stateful Widget.. 상태가 있는 위젯??
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: HomeScreen,
+      home: HomeScreen(),
     );
   }
 }
--- flutter/test_app/lib/screen/screen_home.dart
+++ flutter/test_app/lib/screen/screen_home.dart
@@ -5,21 +5,56 @@ class HomeScreen extends StatefulWidget {
   /*Inheritance임. subclass 기능 추가, HomeScreen은 sub class
   statefulWidget은 base super class*/
   @override
-  _HomeScreenState = createState() => _HomeScreenState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
   @override // getter setter method 가져옴
-  Widger build(BuildContext context) {
+  Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
     double width = screenSize.width;
     double height = screenSize.height;
 
-    return SafeArea( //맨 위와 아래에 간섭하지 않게 하는 것
+    return SafeArea(
+      //맨 위와 아래에 간섭하지 않게 하는 것
       child: Scaffold(
         appBar: AppBar(
           title: Text("flutter chatbot"),
-          backgroundColor: Colors.deeppurple,
-          leading: Container()))
+          backgroundColor: Colors.deepPurple,
+          leading: Container(), //좌측의 버튼을 지움. 뒤로가기 막음
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Center(
+              child: Image.asset(
+                "images/city.png",
+                width: width * 0.8,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(width * 0.024),
+            ),
+            Text(
+              "그 아래에 넣는 단어",
+              style: TextStyle(
+                fontSize: width * 0.065,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "이것이 첫 페이지입니다. 다음은 어떻할까요?",
+              textAlign: TextAlign.center,
+            ),
+            Padding(
+              padding: EdgeInsets.all(width * 0.048),
+
+              //This area is for function
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
